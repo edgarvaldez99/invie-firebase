749c2e5ed9b47300a3947309685c9d46172bd862
Edgar Valdez <edgarvaldez99@gmail.com>
2018-08-03 21:28:14 -0400

    feat: try the methods push, update and set of firebase database

--- js/perfil.js
+++ js/perfil.js
@@ -2,6 +2,12 @@ const btnLogin = document.getElementById("btn-login")
 const btnLogout = document.getElementById("btn-logout")
 const btnLoginConFacebook = document.getElementById("btn-login-con-facebook")
 
+const btnPush = document.getElementById("btn-push")
+const btnUpdate = document.getElementById("btn-update")
+const btnSet = document.getElementById("btn-set")
+
+const refTestDB = firebase.database().ref("test")
+
 const refUserDB = firebase.database().ref("usuario")
 let usuario = {}
 
@@ -64,3 +70,24 @@ function signInWithPopup(provider) {
 function agregarUsuario(usuario) {
     refUserDB.push(usuario)
 }
+
+btnPush.addEventListener("click", event => {
+    refTestDB.push({
+        id: 1,
+        nombre: "test 1"
+    })
+})
+
+btnUpdate.addEventListener("click", event => {
+    refTestDB.child("-LJ1WhEcPeS0XV1I298f").update({
+        nombre: "test 1 actualizado"
+    })
+})
+
+btnSet.addEventListener("click", event => {
+    // este guarda el objeto asi como se le pasa sin generar id y pisa los otros
+    refTestDB.set({
+        id: 2,
+        nombre: "test anterior pisado al usar set"
+    })
+})
