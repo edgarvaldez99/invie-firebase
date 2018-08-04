ac39116a970359978c3ba3232bfe7f7b26248e0d
Edgar Valdez <edgarvaldez99@gmail.com>
2018-08-03 21:32:19 -0400

    refactor: move common functions in index

--- js/index.js
+++ js/index.js
@@ -2,11 +2,20 @@ const btnLogin = document.getElementById("btn-login")
 const btnLogout = document.getElementById("btn-logout")
 const btnLoginConFacebook = document.getElementById("btn-login-con-facebook")
 
+const refUserDB = firebase.database().ref("usuario")
+let usuario = {}
+
 mostrarLogout()
 
 firebase.auth().onAuthStateChanged(user => {
     if (user) {
         mostrarLogout()
+        usuario = {
+            nombre: user.displayName,
+            email: user.email,
+            uid: user.uid
+        }
+        agregarUsuario(usuario)
     } else {
         mostrarLogin()
     }
@@ -16,20 +25,14 @@ btnLogin.addEventListener("click", event => {
     event.preventDefault()
     const provider = new firebase.auth.GoogleAuthProvider()
     provider.addScope('https://www.googleapis.com/auth/contacts.readonly')
-    firebase.auth().languageCode = 'es';
-    firebase.auth().signInWithPopup(provider)
-        .then(datosUsuario => console.log(datosUsuario))
-        .catch(() => console.log("error"))
+    signInWithPopup(provider)
 })
 
 btnLoginConFacebook.addEventListener("click", event => {
     event.preventDefault()
     const provider = new firebase.auth.FacebookAuthProvider()
     provider.addScope('public_profile')
-    firebase.auth().languageCode = 'es';
-    firebase.auth().signInWithPopup(provider)
-        .then(datosUsuario => console.log(datosUsuario))
-        .catch(() => console.log("error"))
+    signInWithPopup(provider)
 })
 
 btnLogout.addEventListener("click", event => {
@@ -49,4 +52,15 @@ function mostrarLogin() {
 function mostrarLogout() {
     btnLoginForEach(btn => btn.parentElement.style.display = "none")
     btnLogout.parentElement.style.display = "block"
+}
+
+function signInWithPopup(provider) {
+    firebase.auth().languageCode = 'es';
+    firebase.auth().signInWithPopup(provider)
+        .then(datosUsuario => console.log(datosUsuario))
+        .catch(error => console.log(error, (error.message ? JSON.parse(error.message) : '')))
+}
+
+function agregarUsuario(usuario) {
+    refUserDB.push(usuario)
 }
