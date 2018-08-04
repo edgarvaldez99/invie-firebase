b1c62218c981ec262c13cf1ce686a3fb299cb901
Edgar Valdez <edgarvaldez99@gmail.com>
2018-08-04 00:44:40 -0400

    feat: update user data from form

--- js/perfil.js
+++ js/perfil.js
@@ -2,6 +2,11 @@ const btnPush = document.getElementById("btn-push")
 const btnUpdate = document.getElementById("btn-update")
 const btnSet = document.getElementById("btn-set")
 
+const btnPerfilEditar = document.getElementById("perfilEditar")
+const btnCancelar = document.getElementById("cancelForm")
+const datosPerfil = document.getElementById("datosPerfil")
+const formularioPerfil = document.getElementById("formularioPerfil")
+
 const refTestDB = firebase.database().ref("test")
 
 btnPush.addEventListener("click", event => {
@@ -42,12 +47,72 @@ firebase.auth().onAuthStateChanged(user => {
 })
 
 function leerDatosUsuario(uid) {
-    refUserDB.child(uid).once("value", data => llenarDatosUsuario(data.val()))
+    refUserDB.child(uid).on("value", data => llenarDatosUsuario(data.val()))
 }
 
 function llenarDatosUsuario(data) {
-    document.getElementById("perfilNombre").innerHTML = data.nombre
-    document.getElementById("perfilEmail").innerHTML = data.email
-    document.getElementById("perfilTelefono").innerHTML = data.telefono || ""
-    document.getElementById("perfilDireccion").innerHTML = data.direccion || ""
+    if (data) {
+        document.getElementById("perfilNombre").innerHTML = data.nombre
+        document.getElementById("perfilEmail").innerHTML = data.email
+        document.getElementById("perfilTelefono").innerHTML = data.telefono || ""
+        document.getElementById("perfilDireccion").innerHTML = data.direccion ? data.direccion.calle+" "+data.direccion.interior+" "+data.direccion.colonia+" CP: "+data.direccion.cp : ""
+    }
+}
+
+function ocultarFormulario() {
+    datosPerfil.style.display = "block"
+    formularioPerfil.style.display = "none"
+}
+
+btnPerfilEditar.addEventListener("click", event => {
+    event.preventDefault()
+    datosPerfil.style.display = "none"
+    formularioPerfil.style.display = "block"
+})
+
+btnCancelar.addEventListener("click", event => {
+    event.preventDefault()
+    ocultarFormulario()
+})
+
+formularioPerfil.addEventListener("submit", event => {
+    event.preventDefault()
+    const user = Array.prototype.map.call(formularioPerfil.querySelectorAll("input"), input => {
+        keys = input.name.split(".")
+        return keys.length > 1 ? { [keys[0]]: { [keys[1]]: input.value }} : { [keys[0]]: input.value }
+    }).reduce((before, current) => mezclarDatos(before, current))
+    console.log(user)
+    refUserDB.child(usuario.uid).update(user)
+    ocultarFormulario()
+})
+
+/**
+ * Simple object check.
+ * @param item
+ * @returns {boolean}
+ */
+function isObject(item) {
+    return (item && typeof item === 'object' && !Array.isArray(item));
+}
+
+/**
+ * Deep merge two objects.
+ * @param target
+ * @param ...sources
+ */
+function mezclarDatos(target, ...sources) {
+    if (!sources.length) return target;
+    const source = sources.shift();
+
+    if (isObject(target) && isObject(source)) {
+      for (const key in source) {
+        if (isObject(source[key])) {
+          if (!target[key]) Object.assign(target, { [key]: {} });
+          mezclarDatos(target[key], source[key]);
+        } else {
+          Object.assign(target, { [key]: source[key] });
+        }
+      }
+    }
+    return mezclarDatos(target, ...sources);
 }
